3271f6b71789d23c143229d810808a449a546125
David Puerto Guerrero <dsp5502@gmail.com>
2022-03-26 20:48:16 -0500

    falta animacion tarjetas

--- src/App.js
+++ src/App.js
@@ -7,6 +7,20 @@ function App() {
       <div className='App2'>
         <h1>WE'RE LAUNCHING SOON</h1>
         <Contador cuentaRegresivaMS={1650997936743} />
+        <div className='iconos'>
+          <img
+            src='https://res.cloudinary.com/djjgtili7/image/upload/v1648320926/counter/icon-facebook_nt2a5u.svg'
+            alt='facebook Icon'
+          />
+          <img
+            src='https://res.cloudinary.com/djjgtili7/image/upload/v1648320926/counter/icon-instagram_wdllu4.svg'
+            alt='Pinterest Icon'
+          />
+          <img
+            src='https://res.cloudinary.com/djjgtili7/image/upload/v1648320926/counter/icon-pinterest_exiwhe.svg'
+            alt='Instagram Icon'
+          />
+        </div>
       </div>
     </div>
   );
--- src/components/counter/Contador.js
+++ src/components/counter/Contador.js
@@ -8,9 +8,12 @@ const DivContainer = styled.div`
   /* border: 1px solid red; */
   font-size: 30px;
   display: flex;
-
   justify-content: center;
   align-items: center;
+
+  @media (max-width: 560px) {
+    width: 100%;
+  }
 `;
 const DivContador = styled.div`
   width: 150px;
@@ -21,19 +24,62 @@ const DivContador = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  @media (max-width: 560px) {
+    width: 100px;
+  }
+`;
+const DivCard = styled.div`
+  width: 100px;
+  height: 100px;
+  /* border: 1px solid red; */
+  font-size: 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(
+    to bottom,
+    hsl(235, 16%, 14%),
+    hsl(236, 21%, 26%)
+  );
+  border-radius: 10px;
+  margin-bottom: 15px;
+
+  @media (max-width: 560px) {
+    width: 70px;
+    height: 70px;
+  }
 `;
 const SpanCounter = styled.span`
-  margin: 0 25px;
+  margin: 0 auto;
   font-size: 10px;
   font-weight: normal;
+  text-transform: uppercase;
+  text-align: center;
+  letter-spacing: 5px;
+
+  @media (max-width: 560px) {
+    font-size: 7px;
+  }
 `;
 const SpanNumeros = styled.span`
   /* border: 1px solid red; */
   width: 100px;
   height: 100px;
   margin: 0 60px;
-  font-size: 80px;
+  font-size: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   text-align: center;
+  font-weight: bold;
+  color: hsl(345, 95%, 68%);
+
+  @media (max-width: 560px) {
+    width: 50px;
+    font-size: 40px;
+  }
 `;
 
 const contadorInicial = {
@@ -42,6 +88,7 @@ const contadorInicial = {
   hours: '00',
   days: '00',
 };
+
 const Contador = ({ cuentaRegresivaMS }) => {
   const [contador, setContador] = useState(contadorInicial);
   const { seconds, minutes, hours, days } = contador;
@@ -60,19 +107,27 @@ const Contador = ({ cuentaRegresivaMS }) => {
   return (
     <DivContainer>
       <DivContador>
-        <SpanNumeros>{days}</SpanNumeros>
+        <DivCard>
+          <SpanNumeros>{days}</SpanNumeros>
+        </DivCard>
         <SpanCounter>days</SpanCounter>
       </DivContador>
       <DivContador>
-        <SpanNumeros>{hours}</SpanNumeros>
+        <DivCard>
+          <SpanNumeros>{hours}</SpanNumeros>
+        </DivCard>
         <SpanCounter>hours</SpanCounter>
       </DivContador>
       <DivContador>
-        <SpanNumeros>{minutes}</SpanNumeros>
+        <DivCard>
+          <SpanNumeros>{minutes}</SpanNumeros>
+        </DivCard>
         <SpanCounter>minutes</SpanCounter>
       </DivContador>
       <DivContador>
-        <SpanNumeros>{seconds}</SpanNumeros>
+        <DivCard>
+          <SpanNumeros>{seconds}</SpanNumeros>
+        </DivCard>
         <SpanCounter>seconds</SpanCounter>
       </DivContador>
     </DivContainer>
